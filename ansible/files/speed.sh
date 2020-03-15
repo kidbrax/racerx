7c3a21f82bc4c5e64066ce83609799d4d3c70b6c
Braxton Beyer <Braxton.Beyer@Stelligent.com>
2020-03-15 18:34:30 -0500

    add aws config and set profile once instead of every command

--- ansible/files/speed.sh
+++ ansible/files/speed.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+export AWS_PROFILE=speedtester
+# export AWS_REGION=us-east-1
+
+
 function manage_log_stream () {
   log_stream_exists=$(aws logs describe-log-streams \
     --log-group-name racerx \
     --log-stream-name-prefix speedtest-results \
-    --profile speedtest \
     | jq '.logStreams | length')
 
   if [ "$log_stream_exists" != 1 ]
@@ -12,22 +15,20 @@ function manage_log_stream () {
     aws logs \
       create-log-stream \
       --log-group-name racerx \
-      --log-stream-name speedtest-results \
-      --profile speedtest
+      --log-stream-name speedtest-results
   fi
 }
 
 function manage_log_group () {
   log_group_exists=$(aws logs describe-log-groups \
     --log-group-name-prefix racerx \
-    --profile speedtest \
     | jq '.logGroups | length')
+
   if [ "$log_group_exists" != 1 ]
   then
     aws logs \
       create-log-group \
-      --log-group-name racerx \
-      --profile speedtest
+      --log-group-name racerx
   fi
 }
 
@@ -48,31 +49,33 @@ function run_speedtest () {
 }
 
 function log_to_cloudwatch () {
+  echo "begin sending log to cloudwatch..."
   next_token=$(aws logs describe-log-streams \
     --log-group-name racerx \
     --log-stream-name-prefix "speedtest-results" \
-    --profile speedtest \
     | jq -r '.logStreams[0].uploadSequenceToken')
+  echo "$next_token"
 
-  if [ "$next_token" == "null" ]
+  if [[ "$next_token" != "null" ]]
+  # if true
   then
-    # echo "token is null"
+    echo "token is $next_token"
     aws logs \
       put-log-events \
       --log-events file://put-events.log \
       --log-group-name racerx \
       --log-stream-name speedtest-results \
-      --profile speedtest
-  else
-    # echo "token is $next_token"
-    aws logs \
-      put-log-events \
-      --log-events file://put-events.log \
-      --log-group-name racerx \
-      --log-stream-name speedtest-results \
-      --profile speedtest \
       --sequence-token "$next_token"
+  else
+    echo "token is null"
+    aws logs \
+      put-log-events \
+      --log-events file://put-events.log \
+      --log-group-name racerx \
+      --log-stream-name speedtest-results
   fi
+
+  echo "Finished sending logs to cloudwatch"
 }
 
 manage_log_group
